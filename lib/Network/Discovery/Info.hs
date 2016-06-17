0624ed3bb4ee82108cf1feafcb56b51473098d99
Patrik Sandahl <patrik.sandahl@gmail.com>
2016-06-17 15:28:54 +0200

    Some non working code.

--- lib/Network/Discovery/Info.hs
+++ lib/Network/Discovery/Info.hs
@@ -5,6 +5,7 @@ module Network.Discovery.Info
     ( Info (..)
     , Interface (..)
     , Protocol (..)
+    , Payload (..)
     , IPAddress
     , PortNum
     ) where
@@ -29,28 +30,53 @@ data Interface = Interface
       -- ^ The role for the interface. Some symbolic role description
       -- selected by the owner of the interface.
     , protocol :: !Protocol
+      -- ^ The protocol used to connect to the interface.
     , address  :: !IPAddress
-    , port     :: !(Maybe PortNum)
+      -- ^ The endpoint IP address for the interface.
+    , port     :: !PortNum
+      -- ^ The endport port number for the interface.
+    , payload  :: !Payload
+      -- ^ The type of payload transported over this interface.
     } deriving (Generic, Show)
 
 data Protocol
     = SCTP
     | TCP
     | UDP
+    | HTTP
+    | HTTPS
+    | Protocol !Text
+    deriving Show
+
+data Payload
+    = HTML
+    | JSON
+    | XML
+    | Payload !Text
     deriving Show
 
 instance FromJSON Interface
 instance ToJSON Interface
 
 instance FromJSON Protocol where
-    parseJSON val@(String s)
-        | "SCTP" == T.toUpper s = pure SCTP
-        | "TCP"  == T.toUpper s = pure TCP
-        | "UDP"  == T.toUpper s = pure UDP
-        | otherwise             = typeMismatch "SCTP, TCP or UDP" val
+    parseJSON (String s)
+        | "SCTP"  == T.toUpper s = pure SCTP
+        | "TCP"   == T.toUpper s = pure TCP
+        | "UDP"   == T.toUpper s = pure UDP
+        | "HTTP"  == T.toUpper s = pure HTTP
+        | "HTTPS" == T.toUpper s = pure HTTPS
+        | otherwise              = pure $ Protocol s
     parseJSON invalid    = typeMismatch "Protocol" invalid
 
 instance ToJSON Protocol where
-    toJSON SCTP = String "SCTP"
-    toJSON TCP  = String "TCP"
-    toJSON UDP  = String "UDP"
+    toJSON SCTP         = String "SCTP"
+    toJSON TCP          = String "TCP"
+    toJSON UDP          = String "UDP"
+    toJSON HTTP         = String "HTTP"
+    toJSON HTTPS        = String "HTTPS"
+    toJSON (Protocol x) = String x
+
+instance FromJSON Payload where
+    parseJSON (String s)
+        | "HTML" == T.toUpper s = pure HTML
+        | "JSON" ==
