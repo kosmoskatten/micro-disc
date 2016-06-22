0ccf4ec0d239725893458b8efc1b2aee522e55f8
Patrik Sandahl <patrik.sandahl@gmail.com>
2016-06-22 13:35:04 +0200

    Moved the discover info record to own library.

---
+++ disc-info/tests/Main.hs
@@ -0,0 +1,18 @@
+module Main
+    ( main
+    ) where
+
+import Test.Framework (Test, defaultMain, testGroup)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+
+import Network.Discovery.InfoProps (encodeDecodeInfo)
+
+main :: IO ()
+main = defaultMain testSuite
+
+testSuite :: [Test]
+testSuite =
+    [ testGroup "JSON encoding/decoding property tests"
+        [ testProperty "Encode/decode of Info records" encodeDecodeInfo
+        ]
+    ]
--- micro-disc/tests/Main.hs
+++ micro-disc/tests/Main.hs
@@ -4,9 +4,7 @@ module Main
 
 import Test.Framework (Test, defaultMain, testGroup)
 import Test.Framework.Providers.HUnit (testCase)
-import Test.Framework.Providers.QuickCheck2 (testProperty)
 
-import Network.Discovery.InfoProps (encodeDecodeInfo)
 import Network.Discovery.RegistryTests ( registerOneService
                                        , reRegisterOneService
                                        , discoverNonExistService
@@ -18,10 +16,7 @@ main = defaultMain testSuite
 
 testSuite :: [Test]
 testSuite =
-    [ testGroup "JSON encoding/decoding property tests"
-        [ testProperty "Encode/decode of Info records" encodeDecodeInfo
-        ]
-    , testGroup "Registry unit tests"
+    [ testGroup "Registry unit tests"
         [ testCase "Register one service" registerOneService
         , testCase "Re-register one service" reRegisterOneService
         , testCase "Discover a non-exist service" discoverNonExistService
