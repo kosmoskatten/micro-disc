fe646f6d78c776b4fd9e02821e860f69b3ce8b99
Patrik Sandahl <kosmoskatten.software@gmail.com>
2016-06-25 21:37:31 +0200

    More comments.

--- example-service/src/Main.hs
+++ example-service/src/Main.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+-- | Example program to show how service discovery and service supervision
+-- can be made using micro-disc and NATS.
 module Main
     ( main
     ) where
@@ -31,32 +34,47 @@ import System.Environment (getArgs)
 
 import qualified Data.ByteString.Char8 as BS
 
+-- | A collection of data used during service runtime.
+data Context = Context
+    { conn :: !Connection
+      -- ^ Connection to NATS.
+    , out  :: !Out
+      -- ^ A handle on which synchronized printing to stdout can
+      -- be made.
+    , kill :: !KillSwitch
+      -- ^ A killswitch, which secondary threads can use to terminate
+      -- the service.
+    }
+
+-- | Commands sent to the killswitch.
 data Heartbeat = Heart | Die
     deriving (Eq, Show)
 
+-- | Convenience type alias for the synchronized stdout channel.
 type Out = MVar ()
-type Kill = MVar Heartbeat
+
+-- | Convencience type alias for the killswitch 'MVar'.
+type KillSwitch = MVar Heartbeat
 
 main :: IO ()
 main = do
     args <- map BS.pack <$> getArgs
     case args of
-        x:y:ys -> initService x y ys
-        _ -> putStrLn "Usage: example-service <nats uri> <name> [services]"
+        nats:name:services -> initService nats name services
+        _                  ->
+            putStrLn "Usage: example-service <nats uri> <name> [services]"
 
 initService :: NatsURI -> ByteString -> [ByteString] -> IO ()
 initService natsUri name services =
     runNatsClient defaultSettings natsUri $ natsConnected name services
 
 natsConnected :: ByteString -> [ByteString] -> Connection -> IO ()
-natsConnected name services conn = do
-    -- Create a simple synchronized stdout writer.
-    out <- newSyncOutput
-    sPutStrLn out $ name `BS.append` " is connected to NATS."
+natsConnected name services conn' = do
+    BS.putStrLn $ name `BS.append` " is connected to NATS."
 
-    -- Create a 'kill switch' where supervision threads can signal to
-    -- main thread to exit.
-    kill <- newKillSwitch
+    -- Create a runtime context for this service.
+    context <- Context conn' <$> newMVar ()
+                             <*> newKillSwitch
 
     -- First register yourselves.
     let info = Info { service = toS name
@@ -64,48 +82,71 @@ natsConnected name services conn = do
                     , interfaces = []
                     }
 
-    pubJson' conn ("service.register." `BS.append` name) info
+    pubJson' conn' (serviceRegister name) info
 
-    -- Subscribe to pings.
-    void $ subAsync' conn (name `BS.append` ".ping") $ handlePing out conn
+    -- Subscribe to pings from the service directory (and others who likes
+    -- to speak with you).
+    void $ subAsync' conn' (selfPing name) $ handlePing context
 
-    -- Discover and supervise the services.
-    mapM_ (discoverAndSupervise out kill conn) services
+    -- Discover and supervise the services specified at the
+    -- command line.
+    mapM_ (discoverAndSupervise context) services
 
-    waitUntilKilled kill
+    -- Wait until a 'Die' message is received, or until someone
+    -- types ^C.
+    waitUntilKilled context
     where
-      waitUntilKilled kill = do
+      waitUntilKilled :: Context -> IO ()
+      waitUntilKilled context@Context {..} = do
           h <- takeMVar kill
-          if h == Heart then waitUntilKilled kill
-                        else putStrLn "Terminating ..."
+          if h == Heart then waitUntilKilled context
+                        else sPutStrLn out "Terminating ..."
 
-discoverAndSupervise :: Out -> Kill -> Connection -> ByteString -> IO ()
-discoverAndSupervise out kill conn name = do
+-- | Discover and supervise a service with the given name.
+discoverAndSupervise :: Context -> ByteString -> IO ()
+discoverAndSupervise context@Context {..} name = do
     sPutStrLn out $ "Waiting to discover " `BS.append` name
-    let topic = "service.discover." `BS.append` name
-    Just (JsonMsg _ _ _ info) <- requestJSON conn topic emptyJSON Infinity
+    Just (JsonMsg _ _ _ info) <-
+        requestJSON conn (serviceDiscover name) emptyJSON Infinity
     sPutStrLn out $ "Done! Got " `BS.append` toS (show (info :: Maybe Info))
-    void $ subAsync' conn ("service.supervise." `BS.append` name) $
-                     handleSupervision out kill name
 
-handlePing :: Out -> Connection -> NatsMsg -> IO ()
-handlePing out conn (NatsMsg _ _ (Just replyTo) payload) = do
+    void $ subAsync' conn (serviceSupervise name) $
+        handleSupervision context name
+
+-- | Handle pings. Just reply with the payload sent.
+handlePing :: Context -> NatsMsg -> IO ()
+handlePing Context {..} (NatsMsg _ _ (Just replyTo) payload) = do
     pub' conn replyTo payload
-    sPutStrLn out "Handled ping"
-handlePing out _ _ = sPutStrLn out "Ping without anywhere to pong"
+    sPutStrLn out "*PING => PONG*"
+handlePing Context {..} _ = sPutStrLn out "Ping without anywhere to pong"
 
-handleSupervision :: Out -> Kill -> ByteString -> NatsMsg -> IO ()
-handleSupervision out kill name _ = do
+-- | Handle the case where a supervised service has gone down. Just
+-- print info and send kill signal to main thread.
+handleSupervision :: Context -> ByteString -> NatsMsg -> IO ()
+handleSupervision Context {..} name _ = do
     sPutStrLn out $ name `BS.append` " has died."
     putMVar kill Die
 
+-- | Helper function to produce the topic of ping to self.
+selfPing :: ByteString -> ByteString
+selfPing name = name `BS.append` ".ping"
+
+-- | Helper functions to produce topic strings.
+serviceRegister, serviceDiscover, serviceSupervise :: ByteString
+                                                   -> ByteString
+serviceRegister = BS.append "service.register."
+serviceDiscover = BS.append "service.discover."
+serviceSupervise = BS.append "service.supervise."
+
+-- | Just to produce some empty JSON data. Should perhaps be available
+-- in nats-client?
 emptyJSON :: [Int]
 emptyJSON = []
 
-newSyncOutput :: IO Out
-newSyncOutput = newMVar ()
-
-newKillSwitch :: IO Kill
+-- | Create a kill switch, a way for a secondary thread to stop the
+-- program. To prevent the GHC runtime to detect "blocking forever on
+-- MVar" a helper thread is producing a 'Heart' value each second.
+newKillSwitch :: IO KillSwitch
 newKillSwitch = do
     kill <- newEmptyMVar
     void $ forkIO $
@@ -114,5 +155,8 @@ newKillSwitch = do
             putMVar kill Heart
     return kill
 
+-- | The example-service is a multithreaded program. To avoid stuff
+-- written to stdout become gibberish a simple 'MVar' synchronization
+-- is made. Otherwise it works like 'BS.putStrLn'.
 sPutStrLn :: Out -> ByteString -> IO ()
-sPutStrLn sync out = withMVar sync $ const (BS.putStrLn out)
+sPutStrLn out str = withMVar out $ const (BS.putStrLn str)
